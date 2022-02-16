915e05c102dc474d91aef3ea829f7a6152da8f05
Kevin Oliveira <kevin.zero@hotmail.com>
2022-02-16 19:27:25 -0300

    Realiza testes para remoção de tarefas

--- src/tests/tasks.test.js
+++ src/tests/tasks.test.js
@@ -10,6 +10,8 @@ jest.mock('axios', () => ({
   create: jest.fn().mockReturnThis(),
   get: jest.fn(() => Promise.resolve()),
   post: jest.fn(() => Promise.resolve()),
+  delete: jest.fn(() => Promise.resolve()),
+  put: jest.fn(() => Promise.resolve()),
 }));
 
 const formElements = () => {
@@ -214,6 +216,80 @@ describe('2 - Renderização de tarefas', () => {
         expect(editBtn).toBeInTheDocument();
         expect(removeBtn).toBeInTheDocument();
       }
+
+    });
+  });
+});
+
+describe('3 - Remoção de tarefas', () => {
+  const tasks = [
+    {
+      _id: "620d6cfe82fa814f0868e3e8",
+      description: "Fazer compras",
+      status: "Pendente",
+      createdAt: "2022-02-15T15:42:03.596Z"
+    },
+    {
+      _id: "620d6cfe82fa814f0868e3e9",
+      description: "Limpar o quarto",
+      status: "Em andamento",
+      createdAt: "2022-02-16T11:07:54.596Z"
+    },
+    {
+      _id: "620d6cfe82fa814f0868e3ea",
+      description: "Consertar carro",
+      status: "Pendente",
+      createdAt: "2022-02-16T21:30:38.596Z"
+    },
+    {
+      _id: "620d6cfe82fa814f0868e3eb",
+      description: "Lavar bicicleta",
+      status: "Pronto",
+      createdAt: "2022-02-14T17:12:21.596Z"
+    },
+  ]
+
+  describe('Remove item da lista de tarefas corretamente', () => {
+    beforeEach(async () => {
+      axios.get.mockImplementationOnce(() =>
+        Promise.resolve({ data: { tasks } }),
+      );
+
+      render(<App />);
+    
+      await waitFor(() => {
+        expect(screen.queryByText("Carregando")).toBe(null);
+      });
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    });
+
+    it('Todas tarefas possuem descrição, status, data, botões para mudança de estado, botão de edição e botão de remoção', async () => {
+      const {     
+        taskDescription,
+        taskStatus,
+        taskDate,
+        readyBtn,
+        ongoingBtn,
+        pendingBtn,
+        editBtn,
+        removeBtn,  } = taskItemsElements(0);
+
+      expect(axios.post).not.toHaveBeenCalled();
+      userEvent.click(removeBtn);
+
+      await waitFor(() => {
+        expect(taskDescription).not.toBeInTheDocument();
+        expect(taskStatus).not.toBeInTheDocument();
+        expect(taskDate).not.toBeInTheDocument();
+        expect(readyBtn).not.toBeInTheDocument();
+        expect(ongoingBtn).not.toBeInTheDocument();
+        expect(pendingBtn).not.toBeInTheDocument();
+        expect(editBtn).not.toBeInTheDocument();
+        expect(removeBtn).not.toBeInTheDocument();
+      });
 
     });
   });
